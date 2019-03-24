afa7a55e1361601a9a63ee8c44ecbf9f031af49f
Tegan <teganoar0@gmail.com>
2019-03-24 11:04:40 +1000

    created a src directory. The name src is commonly used and is a directory containing all source files for project. I have put style.css  inside the src directory is a folder names scss  I have moved files into src and renamed them because I am bundling the  SCSS (CSS) file from the src directory

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import gulp from 'gulp'
+import cssnano from 'gulp-cssnano' /* plugin in to ensure style sheet is as small as possible */
+import sass from 'gulp-sass' /* makes it possible to convert SASS to CSS */
+
+
+/**
+ * SCSS Bundling
+ * Bundles SCSS code to CSS
+ * Exports the Bundled file into dist directory.
+ */
+
+function styles() {
+
+  return gulp.src('./src/style.scss')
+    .pipe(sass({
+      outputStyle: 'uncompressed'
+    }).on('error', function (error) {
+      console.error(`${error.messageFormatted}`)
+      this.emit('end')
+    }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('./dist/css'))
+
+}
+
+/**
+ * Watch SCSS file or changes
+ */
+function watch() {
+  gulp.watch('src/**/*.scss', gulp.series(styles))
+}
+
+/**
+ * Export Tasks
+ * Running `gulp watch` from the CLI will watch and bundle changes
+ * Running `gulp styles` will bundle file
+ *
+ */
+exports.watch = watch
+exports.styles = styles
